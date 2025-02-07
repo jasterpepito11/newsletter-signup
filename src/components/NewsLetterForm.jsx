dd33a66e9f9fd3b41c419a83ee845323a324f645
jasterpepito11 <jasterpepito@gmail.com>
2025-02-07 20:54:36 +0800

    Modified base url and had added deploy script for gh-pages

--- src/components/NewsLetterForm.jsx
+++ src/components/NewsLetterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
